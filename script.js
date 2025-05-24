d3d1061832c31844c3220e84fde61620726ca5f9
Roberto <nanuroberto20@gmail.com>
2025-05-24 21:45:02 +0300

    create grid window and grid boxes

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-const GRID_WIDTH = 600;
-const GRID_HEIGHT = 600;
-const GRID_AREA = GRID_WIDTH * GRID_HEIGHT
-let numOfSmallBoxes = 100;
+const GRID_WIDTH = 500;
+const GRID_HEIGHT = 500;
+let gridSize = 16
 
 const body = document.querySelector("body");
 
 // create a 16x16 grid
-
 const container = document.createElement("div");
-
 body.appendChild(container);
 container.style.display = "flex"
 container.style.flexWrap = "wrap"
@@ -16,19 +13,20 @@ container.style.width = GRID_WIDTH.toString() + "px";
 container.style.height = GRID_WIDTH.toString() + "px";
 container.style.margin = "auto"
 container.style.marginTop = "200px"
+container.style.marginBottom = "200px"
 container.style.border = "1px solid black"
 
-// calculate small boxes width adn height based on number of boxes and grid area
-smallBoxWidth = Math.sqrt(GRID_AREA / numOfSmallBoxes) - 2; // -2 beacuse we have 1px border on top and bottom
-smallBoxHeight = Math.sqrt(GRID_AREA / numOfSmallBoxes) - 2; // -2 beacuse we have 1px border on left and right
-
-for(let i = 0; i < numOfSmallBoxes; i++)
+function createGridBoxes(numOfBoxes)
 {
-    const box = document.createElement("div");
-    box.style.width = smallBoxWidth.toString() + "px";
-    box.style.height = smallBoxHeight.toString() + "px";
-    box.style.border = "1px solid grey";
-    console.log(smallBoxHeight);
-    console.log(smallBoxWidth);
-    container.appendChild(box);
+    let boxSize = 100 / numOfBoxes;
+    for(let i = 0; i < numOfBoxes * numOfBoxes; i++)
+    {
+        const box = document.createElement("div");
+        box.style.width = boxSize.toString() + "%";
+        box.style.height = boxSize.toString() + "%";
+        box.style.border = "1px solid black";
+        container.appendChild(box);
+    }
 }
+
+createGridBoxes(gridSize);
